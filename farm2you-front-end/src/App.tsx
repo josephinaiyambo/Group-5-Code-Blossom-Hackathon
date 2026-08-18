6498e5e6b80fa758f5207e55952a132675e20fac
Lisa Chikovore <lisachikovore@gmail.com>
2026-08-18 10:56:19 +0200

    added hero page, home page and some styling

--- farm2you-front-end/src/App.tsx
+++ farm2you-front-end/src/App.tsx
@@ -1,21 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
-import AddProduct from "./pages/AddProduct";
-import AddNeed from "./pages/AddNeed";
-import Matches from "./pages/Matches";
 
 function App() {
   return (
     <BrowserRouter>
+
       <NavBar />
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/sell" element={<AddProduct />} />
-        <Route path="/buy" element={<AddNeed />} />
-        <Route path="/matches" element={<Matches />} />
       </Routes>
+
     </BrowserRouter>
   );
 }
--- farm2you-front-end/src/components/NavBar.tsx
+++ farm2you-front-end/src/components/NavBar.tsx
@@ -1,10 +1,29 @@
-import {Link} from  'react-router-dom'
+import { Link } from "react-router-dom";
+
 function NavBar() {
   return (
-      <>
+    <nav className="navbar">
+      <div className="navbar-container">
 
-    </>
+        <Link to="/" className="logo">
+          Farm2You
+        </Link>
+
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/marketplace">Marketplace</Link>
+          <Link to="/sell">Sell Produce</Link>
+          <Link to="/buy">Buy Produce</Link>
+          <Link to="/matches">Matches</Link>
+        </div>
+
+        <button className="login-button">
+          Login
+        </button>
+
+      </div>
+    </nav>
   );
 }
 
-export default NavBar
+export default NavBar;
